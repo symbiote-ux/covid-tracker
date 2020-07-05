8cad1a582f35be2c619b61e983d9c638893c3e84
ayush <58028488+desibabua@users.noreply.github.com>
2020-07-05 12:16:51 +0530

    added distict template

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/:district/', (req, res) => {
+  console.log(req.params);
+  res.end();
+})
+
 app.listen(4000, () => console.log('listening on 4000'));
