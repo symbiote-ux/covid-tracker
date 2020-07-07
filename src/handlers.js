8c4fe2d82638efe19012725235061cf92fda7d88
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-07-07 18:20:16 +0530

    added temp

--- src/handlers.js
+++ src/handlers.js
@@ -5,7 +5,18 @@ const { Scheduler } = require('./scheduler');
 const { InfoProvider } = require('./infoProvider');
 const content = require('../database/district.json');
 
+const getWorkerOptions = () => {
+  return {
+    host: 'localhost',
+    port: 5000,
+    method: 'post',
+    path: '/process',
+  };
+};
+
 const infoProvider = new InfoProvider(content);
+const scheduler = new Scheduler(getWorkerOptions());
+scheduler.start();
 
 app.get('/district/:district/', (req, res) => {
   const districtInfo = infoProvider.getDistrictInfo(req.params.district);
