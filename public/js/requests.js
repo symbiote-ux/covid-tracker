bdf24aacf5da1f491bd2182beb72b78d77b6ab67
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-07-12 22:57:30 +0530

    handled error

--- public/js/requests.js
+++ public/js/requests.js
@@ -14,14 +14,25 @@ const sendReq = function (method, url, body, callback) {
   req.send(content);
 };
 
-const updateDisplay = function ({msg,jobId}) {
+const updateDisplay = function ({ msg, jobId }) {
   const message = `<p>${msg}</p>`;
   const board = document.getElementById('board');
   board.innerHTML = message;
-}
+};
 
 const processJob = function () {
   const location = document.getElementById('loc').value;
   const locationName = document.getElementById('locName').value;
-  sendReq('POST','/process',{location,locationName},updateDisplay)
-}
+  sendReq('POST', '/process', { location, locationName }, updateDisplay);
+};
+
+const displayProcessedData = function (result) {
+  const message = `<p>${JSON.stringify(result)}</p>`;
+  const board = document.getElementById('board');
+  board.innerHTML = message;
+};
+
+const getProcessedJob = function () {
+  const jobId = document.getElementById('jobId').value;
+  sendReq('POST', '/getInfo', { jobId }, displayProcessedData);
+};
